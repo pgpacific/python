0252ac3d1b2e78d33023de92f0e84799a9c39e4a
Prashant Joshi <prashant.joshi@leapscale.com>
2022-03-15 19:02:26 +0530

    advice to students

--- hw.py
+++ hw.py
@@ -5,3 +5,6 @@ First program Hello World
 print("Hello World")
 print("\nWelcome to the world of python")
 print("\nLearning begins by doing")
+
+print("\n**** All said and done, programming needs good design and then elegant code ****")
+print("\n**** Only diligent and focused practise can get you to that ****")
